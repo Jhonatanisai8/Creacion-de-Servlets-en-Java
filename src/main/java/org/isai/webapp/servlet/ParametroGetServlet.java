115d0ad08db8c5fd72c926590dc29496d6fff1e9
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-16 10:42:47 -0500

    Enviando parametros GET de la url o Query String

--- src/main/java/org/isai/webapp/servlet/ParametroGetServlet.java
+++ src/main/java/org/isai/webapp/servlet/ParametroGetServlet.java
@@ -1,31 +1,32 @@
 package org.isai.webapp.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "HolaMundoServlet", urlPatterns = {"/HolaMundoServlet"})
-public class HolaMundoServlet extends HttpServlet {
-
+@WebServlet("/parametros/url-get")
+public class ParametroGetServlet extends HttpServlet {
+    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        String saludo = request.getParameter("saludo");
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet HolaMundoServlet</title>");
+            out.println("<title>Servlet Parametros del request </title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Mi primer servlet</h1>");
+            out.println("<h1>Servlet Parametros del request </h1>");
+            out.println("<h2> El saludo enviado es: " + saludo + "</h2>");
             out.println("</body>");
             out.println("</html>");
         }
     }
-
+    
 }
